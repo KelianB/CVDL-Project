744c19fea3633c9c59e4414509b294e2ff644597
KelianB <kelian.baert@gmail.com>
2020-04-03 11:46:06 +0000

    Added final model from assignment 4

--- SSD/ssd/modeling/backbone/basic.py
+++ SSD/ssd/modeling/backbone/basic.py
@@ -1,7 +1,8 @@
 import torch
+from torch import nn
+from ssd import torch_utils
 
-
-class BasicModel(torch.nn.Module):
+class BasicModel(nn.Module):
     """
     This is a basic backbone for SSD.
     The feature extractor outputs a list of 6 feature maps, with the sizes:
@@ -20,7 +21,135 @@ class BasicModel(torch.nn.Module):
         self.output_channels = output_channels
         image_channels = cfg.MODEL.BACKBONE.INPUT_CHANNELS
         self.output_feature_size = cfg.MODEL.PRIORS.FEATURE_MAPS
-    
+
+        # Task 4a backbone
+        """
+        feature_bank_extractors = nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(in_channels=image_channels, out_channels=32, kernel_size=3, stride=1, padding=1),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=64, out_channels=output_channels[0], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[0], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=128, out_channels=output_channels[1], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[1], out_channels=256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=256, out_channels=output_channels[2], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[2], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=128, out_channels=output_channels[3], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[3], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=128, out_channels=output_channels[4], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[4], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=128, out_channels=output_channels[5], kernel_size=3, stride=2, padding=0),
+            )
+        )
+       """
+
+        # Custom backbone
+        feature_bank_extractors = nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(in_channels=image_channels, out_channels=16, kernel_size=4, stride=1, padding=2),
+                nn.ReLU(),
+
+                nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+
+                nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.ReLU(),
+                
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=1, padding=2),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.ReLU(),
+                
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=64, out_channels=output_channels[0], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Dropout2d(p=0.2),
+                nn.Conv2d(in_channels=output_channels[0], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+
+                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=5, stride=1, padding=2),
+                nn.ReLU(),
+                
+                nn.Conv2d(in_channels=128, out_channels=output_channels[1], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(in_channels=output_channels[1], out_channels=256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                
+                nn.Conv2d(in_channels=256, out_channels=output_channels[2], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(in_channels=output_channels[2], out_channels=256, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(256),
+                nn.ReLU(),
+                
+                nn.Conv2d(in_channels=256, out_channels=output_channels[3], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Dropout2d(p=0.1),
+                nn.Conv2d(in_channels=output_channels[3], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+
+                nn.Conv2d(in_channels=128, out_channels=output_channels[4], kernel_size=3, stride=2, padding=1),
+            ),
+            nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels=output_channels[4], out_channels=128, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                
+                nn.Dropout2d(p=0.2),
+                nn.Conv2d(in_channels=128, out_channels=output_channels[5], kernel_size=3, stride=2, padding=0),
+            )
+        )
+        
+        
+        self.feature_bank_extractors = torch_utils.to_cuda(feature_bank_extractors)
+
     def forward(self, x):
         """
         The forward functiom should output features with shape:
@@ -34,8 +163,15 @@ class BasicModel(torch.nn.Module):
         where out_features[0] should have the shape:
             shape(-1, output_channels[0], 38, 38),
         """
+        
         out_features = []
+        for seq in self.feature_bank_extractors:
+            x = seq(x)
+            out_features.append(x)
+        
         for idx, feature in enumerate(out_features):
+            out_channel = self.output_channels[idx]
+            feature_map_size = self.output_feature_size[idx] 
             expected_shape = (out_channel, feature_map_size, feature_map_size)
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
